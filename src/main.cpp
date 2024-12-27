c48a87b586b83c662f706193e92dc66da95ff81d
Sophie <65089742+sophie-72@users.noreply.github.com>
2024-12-27 11:05:34 -0500

    add main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <Arduino.h>
+#include "Robot.h"
+#include "Motor.h"
+#include "UltrasonicSensor.h"
+
+constexpr int leftMotorForwardPin = 8;
+constexpr int leftMotorBackwardPin = 7;
+constexpr int leftMotorSpeedPin = 9;
+
+constexpr int rightMotorForwardPin = 5;
+constexpr int rightMotorBackwardPin = 4;
+constexpr int rightMotorSpeedPin = 3;
+
+constexpr int ultrasonicSensorTriggerPin = 11;
+constexpr int ultrasonicSensorEchoPin = 10;
+
+Motor leftMotor(leftMotorForwardPin, leftMotorBackwardPin, leftMotorSpeedPin);
+Motor rightMotor(rightMotorForwardPin, rightMotorBackwardPin, rightMotorSpeedPin);
+UltrasonicSensor ultrasonicSensor(ultrasonicSensorTriggerPin, ultrasonicSensorEchoPin);
+Robot robot(leftMotor, rightMotor, ultrasonicSensor);
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    Serial.println("Robot Initialized!");
 }
 
 void loop() {
-// write your code here
+    robot.move();
+    delay(500);
 }
