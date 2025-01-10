36ecef26f21e62ca0ed2967180a9024aa034ae04
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-01-09 21:27:25 -0500

    remove unused if statement

--- src/UltrasonicSensor.cpp
+++ src/UltrasonicSensor.cpp
@@ -18,9 +18,5 @@ float UltrasonicSensor::getDistanceInCentimeters() const {
 
     const unsigned long duration = pulseIn(echoPin, HIGH);
 
-    if (duration == 0) {
-        return -1.0f;
-    }
-
     return (static_cast<float>(duration) / 2.0f) * SPEED_OF_SOUND_IN_CENTIMETERS_PER_MICROSECOND;
 }
